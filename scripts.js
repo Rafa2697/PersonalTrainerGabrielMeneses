e7001c4c7fbfb18c9f2e35c057520467a7640fc4
Rafael <Rafael.mattos468@gmail.com>
2024-03-31 11:54:04 -0300

    style: improvements of styles at the button and responsivity the menu

--- scripts.js
+++ scripts.js
@@ -3,11 +3,15 @@
 //efito do botão nav
 
 let navList = document.querySelector('.nav_list');
-document.querySelector('.bi-list').addEventListener('click', () => {
+let botao = document.querySelector('.bi-list');
+botao.addEventListener('click', () => {
     if(navList.style.display === 'none'){
         navList.style.display = 'flex'
+        botao.style.transform = 'rotate(90deg)';
+        botao.style.transition = '.5s ease-in-out' 
     }else{
         navList.style.display = 'none'
+        botao.style.transform = 'rotate(0)'
     }
 })
 
